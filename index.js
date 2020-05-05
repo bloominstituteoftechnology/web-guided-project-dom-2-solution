7847ff9b4393e93e49e79cd627e8a3ed7dcbae53
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-05 16:28:51 +0200

    update instructions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('project is up')
 
-// 1- Create a function called 'get'
+// 👉 TASK 1- Create a function called 'get'
 // that takes a CSS selector as its argument
 // and executes document.querySelector with the argument.
 const get = selector => {
@@ -8,7 +8,7 @@ const get = selector => {
 }
 
 
-// 2- Using 'get', select the following elements from the DOM:
+// 👉 TASK 2- Using 'get', select the following elements from the DOM:
 
 // BUTTONS
 const launchButton = get('#launchButton')
@@ -24,7 +24,7 @@ const modal = get('div.modal')
 const modalOpacity = get('div.modal-opacity')
 
 
-// 3- Create a function that launches!
+// 👉 TASK 3- Create a function that launches!
 // It should open the confirmation modal.
 // Add it as an event listener for click events on the launch button.
 function launch(event) {
@@ -35,7 +35,7 @@ function launch(event) {
 launchButton.addEventListener('click', launch)
 
 
-// 4- Create a function to confirm the launch.
+// 👉 TASK 4- Create a function to confirm the launch.
 // It should close the modal and display a success report.
 // Add it as a listener for clicks on the confirmation button.
 function confirm(event) {
@@ -44,7 +44,7 @@ function confirm(event) {
 }
 confirmButton.addEventListener('click', confirm)
 
-// 5- Create a function to cancel the launch.
+// 👉 TASK 5- Create a function to cancel the launch.
 // It should close the modal and display a failure report.
 // Add it as a listener for clicks on the cancellation button.
 function cancel(event) {
@@ -54,7 +54,7 @@ function cancel(event) {
 cancelButton.addEventListener('click', cancel)
 
 
-// 6- Create a function that closes the modal if
+// 👉 TASK 6- Create a function that closes the modal if
 // the user hits the Escape key on their keyboard.
 // Add it as an event listener for 'keydown' events on document.
 function escKey(event) {
@@ -65,7 +65,7 @@ function escKey(event) {
 document.addEventListener('keydown', escKey)
 
 
-// 7- Add to ALL ELEMENTS ON THE PAGE an event listener for click events.
+// 👉 TASK 7- Add to ALL ELEMENTS ON THE PAGE an event listener for click events.
 // It should console.log the target 🎯 of the event.
 // It should also console.log the CURRENT target 🧭 of the event.
 // Play with stopPropagation and stopImmediatePropagation.
@@ -81,15 +81,15 @@ modal.addEventListener('click', event => {
 })
 
 
-// 8- Create a link and prevent its default behavior
+// 👉 TASK 8- Create a link and prevent its default behavior
 
 
-// 9- Using the mouseOver event on 'document',
+// 👉 TASK 9- Using the mouseOver event on 'document',
 // log to the console the X and Y coordinates
 // of the mouse pointer, as it moves over the screen.
 
 
-// 10- [STRETCH] Create helper functions
+// 👉 TASK 10- [STRETCH] Create helper functions
 function openModal() {
 
 }
